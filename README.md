a6fc59ccc4b8dada9834a762840b7802f4b3d621
Louis Gombert <louisgombert@gmail.com>
2021-05-23 13:15:07 +0200

    MULTIPLAYER SUPPORT

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 ```bash
 pip install -r requirements.txt
 ```
+
+Jouer contre l'IA
+```bash
+python main.py <compte> ai <nom_ai>
+```
+compte : 1, 2 ou 3
+
+Jouer en PvP
+Créer la salle
+```bash
+python main.py <compte> pvp_create <salle>
+```
+
+Rejoindre une salle (APRES l'avoir créée)
+```bash
+python main.py <compte> pvp_join <salle>
+```
--- main.py
+++ main.py
@@ -1,17 +1,35 @@
+import json
+import sys
+
 from src.io import Connection
 from src.player import Player
 from src.carte import Carte
 from src.gameviz.logger import GameLogger
+
+if len(sys.argv) != 4:
+    print("3 args, cf readme")
+
+password = ["", "g[B>!&I7C#V;-Y,OW%+/9A5", "ET1j]ZWe(JY)^A4_#@_1.h_J", "0%%IFIJ^&_Ac#_>R,a_YA+'"]
+account = int(sys.argv[1])
+mode = sys.argv[2]
+
 connexion = Connection()
+# connexion.login("Lyon3", password[3])
+connexion.login(f"Lyon{account}", password[account])
 
-password = ["g[B>!&I7C#V;-Y,OW%+/9A5", "ET1j]ZWe(JY)^A4_#@_1.h_J", "0%%IFIJ^&_Ac#_>R,a_YA+'"]
+if mode == "ai":
+    print(f"Nouvelle partie AI avec l'IA {sys.argv[3]}")
+    connexion.newGame(sys.argv[3])
+elif mode == "pvp_create":
+    print(f"Nouvelle partie PVP salle {sys.argv[3]}")
+    connexion.newGameMJ(sys.argv[3])
+elif mode == "pvp_join":
+    print(f"Rejoint la salle PVP {sys.argv[3]}")
+    connexion.joinGame(sys.argv[3])
 
-connexion.login("Lyon3", password[2])
-connexion.deleteGames()
-
-connexion.newGame('RessourceAI')
 connexion.connect()
 turn1 = connexion.getMap()
+
 map = Carte(turn1)
 
 logger = GameLogger("game_logs/summon_game.json")
@@ -24,33 +42,41 @@ if turn1["your_turn"]:
 else:
     print("On joue en 2e")
 
-for turn_number in range(1001):
+for turn_number in range(10000):
     print(f"Tour #{turn_number}")
-    
+
     # Recupere les données du tour
     data = connexion.getTurn()
+    data = [json.loads(d) for d in data.split("\n")[:-1]]
 
-    # La partie est terminée
-    if "your_turn" not in data:
-        print("Fin de la partie")
-        if data["AIsWinner"] == True:
-            print("A a gagné")
-        else:
-            print("A a perdu")
+    # Si on reçoit plusieurs tours, on les traite 1 par 1
+    for d in data:
+        if "errors" in d:
+            print("Erreurs !")
+            print(d["errors"])
+
+        # La partie est terminée
+        if "your_turn" not in d:
+            print("Fin de la partie")
+            if d["AIsWinner"] == True:
+                print("A a gagné")
+            else:
+                print("A a perdu")
+            break
+
+        player.update(d)
+
+    print(data)
+    if len(data) == 2:
         break
 
-    player.update(data)
-
-    if data["your_turn"]==False:
+    if data[-1]["your_turn"] == False:
         continue
 
     # On joue
     played = player.play()
     connexion.sendTurn(played)
     logger.log_gamestate(player.game_map)
-
-    if turn_number > 20:
-        break
 
 connexion.deleteGames(connexion.game_id)
 logger.save_logs()

42a18e1b374226daa8c2a0f6b431ccd8c17935bc
Arthur Durand <62608501+ArthurDurand@users.noreply.github.com>
2021-05-23 11:42:10 +0200

    ajout attaque & miner dans util

--- src/player.py
+++ src/player.py
@@ -1,3 +1,5 @@
+import src.util
+
 import json
 class Player:
     def __init__(self, game_map):
@@ -46,23 +48,29 @@ class Player:
 
                 # Si il est en train de miner
                 if unite.role == 2:
-                    # On regarde quel voisin est une ressource
+                    # On mine
+                    posRessource = src.util.miner(unite,self.game_map)
+
+                    '''
                     voisins = self.game_map.adjacent(unite.position.x,unite.position.y)
                     for v in voisins:
                         if self.game_map.terrain[v[0],v[1]] == 3:
                             # On ajoute à la liste de minage
                             self.mine[str(self.game_map.position_serial_to_UD(unite.position[0], unite.position[1]))] = self.game_map.position_serial_to_UD(v[0], v[1])
                             break
+                    '''
+                    
                             
                 # Si il se déplace vers la ressource
                 if unite.role == 1:
-                    import src.util.nextPositions
-                    moves = nextPositions((unite.position[0], unite.position[1]), self.game_map, (unite.target[0], unite.target[1], unite.pointMouvement)
-                    self.moves[]
                     #continuer déplacement
+                    moves = src.util.nextPositions((unite.position[0], unite.position[1]), self.game_map, (unite.target[0], unite.target[1], unite.pointMouvement))
+                    '''
+                    turn.move(unite.position, moves)
+                    '''
                 
                         
-
+                # TODO PLUS TARD
                 #Si il se déplace pour construire un amphi
                 #if unite.role == 3:
                     #si sur case target
@@ -72,13 +80,24 @@ class Player:
 
                 #Si il n'a pas de role
                 if unite.role == 0:
-                    pass
+                    
+                    # TODO PLUS TARD
                     #si gold > 250 + x
                         # Affecter a rôle 3
                         # Donne les coordonnees de la target
                     
-                    #else
-                        #affecter a role 1 et trouver la ressource libre la plus proche
+                    # Trouve la ressource libre la plus proche
+                    posLibreRessource = src.util.closestAvailableRessource(unite, self.game_map)
+                    unite.target = posLibreRessource
+                    # on save que cette position est prise
+                    self.game_map.target[posLibreRessource[0]][posLibreRessource[1]]=True
+                    # on deplace
+                    deplacementUnite = src.util.nextPositions(unite.position,self.game_map,unite.target,unite.pointMouvement)
+                    '''turn.move(unite,deplacementUnite)'''            
+                    # on passe au role "se deplacer vers ress"        
+                    unite.role = 1
+
+                    #affecter a role 1 et trouver la ressource libre la plus proche
 
         # Summon le max d'ingés (autour du spawn)
         # Mise à jour de la liste d'ingés
